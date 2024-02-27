54f56e961bd8460a1f73b70522cbe17e359a462f
Ari Abrams <ariabrams65@gmail.com>
2024-02-26 21:13:23 -0800

    Calculator is almost done

--- Calculator/main.c
+++ Calculator/main.c
@@ -16,7 +16,7 @@ char get_pressed_button() {
 	char output = BIT_0;
 	for (; output <= BIT_3; output <<= 1) {
 		GPIO_PORTF_DATA_R = output;			
-		char input = GPIO_PORTD_DATA_R;
+		char input = GPIO_PORTD_DATA_R & 0xf;
 		if (input > 0x00) {
 			return matrix[bit_map[output]][bit_map[input]];				
 		}
@@ -24,13 +24,45 @@ char get_pressed_button() {
 	return '\0';
 }
 
+void UART_setup() {
+    // 1. Enable the UART module using the RCGCUART register (see page 344).
+    SYSCTL_RCGCUART_R = BIT_0;
+
+
+    // 3. Set the GPIO AFSEL bits for the appropriate pins (see page 671). To determine which GPIOs to
+    // configure, see Table 23-4 on page 1344.
+    GPIO_PORTA_AFSEL_R = BIT_0 | BIT_1;
+
+    // 5. Configure the PMCn fields in the GPIOPCTL register to assign the UART signals to the appropriate
+    // pins (see page 688 and Table 23-5 on page 1351).
+    GPIO_PORTA_PCTL_R = BIT_0 | BIT_4;
+    
+    GPIO_PORTA_DEN_R = BIT_0 | BIT_1;
+
+    UART0_CTL_R &= ~BIT_0; //clears the first bit in UARTCTL
+    UART0_IBRD_R = 104;
+    UART0_FBRD_R = 11;
+    UART0_LCRH_R = UART_LCRH_WLEN_8;
+    UART0_CC_R = 0x0; 
+    
+    UART0_CTL_R = BIT_0 | BIT_8 | BIT_9;
+}
+
+void UART_Tx(char data) {
+    while (UART0_FR_R & BIT_5);
+        UART0_DR_R = data;
+}
+
+
 char get_pressed_button_blocking() {
-	char button = get_pressed_button();
-	while (button == '\0') {
-		button = get_pressed_button();
+	while(1) {
+		char button = get_pressed_button();
+		if (button != '\0') {
+			while (get_pressed_button() != '\0');
+			return button;
+		}
 	}
-	while(get_pressed_button() != '\0');	
-	return button;
+	
 }
 
 void set_RS(unsigned char rs) {
@@ -129,14 +161,14 @@ void init_LCD() {
 }
 
 void init_ports() {
-	SYSCTL_RCGCGPIO_R |= BIT_1 | BIT_3 | BIT_4 | BIT_5;
+	SYSCTL_RCGCGPIO_R |= BIT_0 | BIT_1 | BIT_3 | BIT_4 | BIT_5;
 	
 	GPIO_PORTB_DEN_R |= 0xff;
 	GPIO_PORTB_DIR_R |= 0xff;
 
 	GPIO_PORTD_DEN_R |= 0xf;
 	GPIO_PORTD_DIR_R &= ~0xf;
-	GPIO_PORTD_PDR_R |= 0xf;
+	// GPIO_PORTD_PDR_R |= 0xf;
 
 	GPIO_PORTE_DEN_R |= 0x07;
 	GPIO_PORTE_DIR_R |= 0x07;
@@ -147,6 +179,15 @@ void init_ports() {
 
 void cursor_to_bottom_row() {
 	write_instruction(0xC0);	
+}
+
+void calculate(char* A, char* B) {
+	clear_display();
+	cursor_to_bottom_row();
+	char product[17];
+	// itoa(atoi(A) * atoi(B), product, 10);
+	sprintf(product, "%d", atoi(A) * atoi(B));
+	write_str(product);
 }
 
 void run_calculator() {
@@ -162,8 +203,9 @@ void run_calculator() {
 			i = 0;
 			clear_display();	
 		}	
-		if (button == '#') {
-			break;	
+		if (button == '#' && state != 'D') {
+			calculate(A, B);
+			state = 'D';
 		}	
 		if (button == 'C') {
 			A = "0\0\0\0\0\0\0\0";
@@ -175,30 +217,29 @@ void run_calculator() {
 
 		if (state == 'A') {
 			A[i] = button;			
-		} else {
+			write_char(button);
+		} else if (state == 'B') {
 			B[i] = button;	
+			write_char(button);
 		}
-		if (i == 8) {
+		if (i == 7) {
 			if (state == 'A') {
 				state = 'B';
 				i = 0;
 				clear_display();	
-			} else {
-				break;	
+			} else if (state == 'B') {
+				calculate(A, B);
+				state = 'D';
 			}
 		}
+		++i;
 	}
-	clear_display();
-	cursor_to_bottom_row();
-	char product[17];
-	// itoa(atoi(A) * atoi(B), product, 10);
-	sprintf(product, "%d", atoi(A) * atoi(B));
-	write_str(product);
 }
 
 int main(void)
 {
 	init_ports();
+	UART_setup();
 	init_LCD();
 	run_calculator();
 
